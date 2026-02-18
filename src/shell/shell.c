ff8845a4138d483ab5ed5220a247ac22e144a33d
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-02-18 23:34:21 +0530

    Addtion of new cmd and changed welcome message

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -10,23 +10,36 @@ void shell_execute(const char* command)
     if (command[0] == '\0')
         return;
 
+    // echo command
     if (strncmp(command, "echo ", 5) == 0)
     {
-        // Print everything after "echo "
         print(command + 5);
         print("\n");
         return;
     }
 
+    // help command
     if (strncmp(command, "help", 4) == 0)
     {
-        print("Commands:\nhelp\nclear\necho\n");
+        print("Available commands:\n");
+        print("help  - Show commands\n");
+        print("clear - Clear screen\n");
+        print("echo  - Print text\n");
+        print("version - Kernel info\n");
         return;
     }
 
+    // clear command
     if (strncmp(command, "clear", 5) == 0)
     {
         terminal_intialize();
+        return;
+    }
+
+    if (strncmp(command, "version", 7) == 0)
+    {
+        print("Kernix Kernel v0.1\n");
+        print("32-bit\n");
         return;
     }
 
