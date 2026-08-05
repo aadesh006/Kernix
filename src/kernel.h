f435d8c1e4a6de162693a476a89c94efdd5db67c
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-08-05 10:43:26 +0530

    Implement heap management in kernel: initialize heap structure and add kernel_get_heap function

--- src/kernel.h
+++ src/kernel.h
@@ -1,5 +1,6 @@
 #ifndef KERNEL_H
 #define KERNEL_H
+#include <stdint.h>
 
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 20
@@ -7,8 +8,10 @@
 void kernel_main();
 void print(const char* str);
 void panic(const char* msg);
+struct heap* kernel_get_heap(void);
 
 void terminal_writechar(char c, char colour);
 void terminal_backspace();
+extern uint32_t _kernel_end;
 
 #endif
